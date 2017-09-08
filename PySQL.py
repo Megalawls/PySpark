01fdbfe66832dc4853928105fb5353a48118ccce
Megalawls <daniel.russell@jrweb.org.uk>
2017-09-08 05:58:35 -0700

    Add files via upload
    Added a lot of SQL-based spark queries

--- PySQL.py
+++ PySQL.py
@@ -1,29 +1,71 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import *
-from pyspark.sql.functions import col
-import pyspark.sql.functions as sqlfn
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
 
 config=SparkConf()
 config.setMaster("local")
 sc=SparkContext(conf=config)
 sql=SQLContext(sc)
 
-records = sc.textFile("ratings.txt")\
-.map(lambda record: record.split("|"))
+movies = sc.textFile("file:///home/cloudera/Desktop/Python/item.txt")\
+	.map(lambda record: record.split("|"))\
+	.map(lambda record: [int(record[0]), record[1]]).collect()
 
-DF1 = sql.createDataFrame(records)
+records = sc.textFile("file:///home/cloudera/Desktop/Python/ratings.txt")\
+.map(lambda record: record.split("|"))\
+.map(lambda record: [int(record[0]), int(record[1]), int(record[2]), int(record[3])])
 
-#print("--- DF1.printSchema")
-#DF1.printSchema()
+#Creates the DataFrame, parsing all elements to the types specified, naming columns also.
+schema=StructType([\
+StructField("userid",LongType(),True),\
+StructField("filmid", LongType(),True),\
+StructField("rating", LongType(),True),\
+StructField("timestamp", LongType(),True)])
+
+#Can be used instead, just to label the columns
+#columnnames = ["userid","filmid","rating","timestamp"]
+
+movieschema=StructType([
+StructField("filmid", LongType(), True),
+StructField("filmname", StringType(), True)])
+
+DF1 = sql.createDataFrame(records, schema)
+DF2 = sql.createDataFrame(movies, movieschema)
 
 #print("--- DF1.show()")
-#]DF1.show(20) 									#Prints 20 records
-#print(DF1.count()) 								#Returns total records
-#DF1.select("_1", "_2").show() 					#Shows Column _1 and _1
-#DF1.select(DF1._1, DF1._2).show()				#Does the same
-#DF1.select(DF1._1).filter(DF1._1>100).show()	#Shows results where column _1 is >100. Automatically parsed from String
+#DF1.show(20) 							#Prints 20 records
+#print(DF1.count()) 						#Returns total records
+#DF1.select("userid", "filmid").show() 				#Shows Column _1 and _2
+#DF1.select(DF1.userid, DF1.filmid).show()			#Does the same
+#DF1.select(DF1.userid).filter(DF1.userid>100).show()		#Shows results where column _1 is >100. Automatically parsed from String
 
-DF1\
-.select(DF1._3.alias("Rating"), DF1._2.alias("Film"))\
-.filter(DF1._2 == 90)\
-.show()
+#Counts all the ratings of film 90, Blade Runner
+RatingsCount =(DF1.select(DF1.rating, DF1.filmid)	#Reduces the columns to just rating and filmid
+	.filter(DF1.filmid == 90)			#filters where the filmid is 90, this is "Blade Runner"
+	.groupBy("rating")				#Groups by rating, the below is required to make anything meaningful of this.
+	.count())					#Counts the occurrence of each rating
+
+RatingsCount.show()					#Displays the transformed data
+
+#Counts how many distinct users and films there are
+print("Distinct Users: " + str(DF1.select(DF1.userid)
+		.distinct()
+		.count()))
+
+print("Distinct Films: " + str(DF1.select(DF1.filmid)
+		.distinct()
+		.count()))
+
+#Counts all the ratings of films
+AverageRatings =(DF1.select(DF1.rating, DF1.filmid)		#Reduces the columns to just rating and filmid
+		.groupBy("filmid")				#Groups by rating, the below is required to make anything meaningful of this.
+		.avg("rating")					#Counts the occurrence of each rating
+		.join(DF2, DF1.filmid == DF2.filmid, 'inner')	#Joins the two created dataframes together, using filmid for reference
+		.drop("filmid")					#Drops both duplicate versions of filmid, dropDuplicates() removes duplicate rows, not duplicate columns
+		.sort(desc("avg(rating)")))			#Orders By Rating
+
+AverageRatings.show(20)						#Displays the transformed data AverageRatings.count()
+
+
+
---
+++ TempTablesandSQL.py
@@ -0,0 +1,59 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import *
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+config=SparkConf()
+config.setMaster("local")
+sc=SparkContext(conf=config)
+sql=SQLContext(sc)
+
+categoriesrdd = sc.textFile("file:///home/cloudera/Desktop/Python/products/category.txt")\
+.map(lambda record: record.split("|"))
+
+subcategoriesrdd = sc.textFile("file:///home/cloudera/Desktop/Python/products/subcategory.txt")\
+.map(lambda record: record.split("|"))
+
+productsrdd = sc.textFile("file:///home/cloudera/Desktop/Python/products/products.txt")\
+.map(lambda record: record.split("|"))
+
+salesrdd = sc.textFile("file:///home/cloudera/Desktop/Python/products/sales.txt")\
+.map(lambda record: record.split("|"))\
+.map(lambda record: [record[0], record[1], int(record[2]), float(record[3])])
+
+categoriesSchema=StructType([
+	StructField("catid", StringType()),
+	StructField("catname", StringType())
+])
+
+subcategoriesSchema=StructType([
+	StructField("catid", StringType()),
+	StructField("subcatid", StringType()),
+	StructField("subcatname", StringType())
+])
+
+productsSchema=StructType([
+	StructField("prodid", StringType()),
+	StructField("prodname", StringType()),
+	StructField("subcatid", StringType())
+])
+
+salesSchema=StructType([
+	StructField("salesid", StringType()),
+	StructField("prodid", StringType()),
+	StructField("quantity", LongType()),
+	StructField("posale", FloatType())
+])
+
+categories = sql.createDataFrame(categoriesrdd, categoriesSchema)
+subcategories = sql.createDataFrame(subcategoriesrdd, subcategoriesSchema)
+products = sql.createDataFrame(productsrdd, productsSchema)
+sales = sql.createDataFrame(salesrdd, salesSchema)
+
+categories.registerTempTable("categories")
+subcategories.registerTempTable("subcategories")
+products.registerTempTable("products")
+sales.registerTempTable("sales")
+
+sql.sql("select prodname,\
+	 count(quantity) as totalsales, sum(quantity) as totalsold, round(sum(posale), 2) as totalincome from sales INNER JOIN products ON sales.prodid=products.prodid	group by prodname order by totalsales desc").show()
